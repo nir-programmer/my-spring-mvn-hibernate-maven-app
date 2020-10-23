57fffd2097a100191a4d8f1e862f3d4de1db85e3
nir <niritzhak10@gmail.com>
2020-10-23 10:06:18 +0300

    Add hello method to the controller

--- src/main/java/org/nir/springdemo/CustomerController.java
+++ src/main/java/org/nir/springdemo/CustomerController.java
@@ -1,8 +1,17 @@
 package org.nir.springdemo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/customers")
 public class CustomerController
 {
-	
+	public String hello(Model model )
+	{
+		model.addAttribute("Hello World Nir SPRPING MVC");
+		return "hello";
+	}
 
 }
