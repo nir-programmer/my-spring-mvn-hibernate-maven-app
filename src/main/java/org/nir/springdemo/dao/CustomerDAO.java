8808a3b83c46938841b273be085a3f2a6497ecf3
nir <niritzhak10@gmail.com>
2020-10-23 14:26:57 +0300

    nothing

--- src/main/java/org/nir/springdemo/dao/CustomerDAO.java
+++ src/main/java/org/nir/springdemo/dao/CustomerDAO.java
@@ -1,7 +1,11 @@
 package org.nir.springdemo.dao;
 
+import java.util.List;
+
+import org.nir.springdemo.entity.Customer;
+
 public interface CustomerDAO 
 {
-	public List<Customer> listCustomer();
+	public List<Customer> getCustomers();
 
 }
