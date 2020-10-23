d724906d6b1cbc5cbbb77c48494b48a51ed9657b
nir <niritzhak10@gmail.com>
2020-10-23 14:02:01 +0300

    nothing

--- src/main/java/org/nir/springdemo/entity/Customer.java
+++ src/main/java/org/nir/springdemo/entity/Customer.java
@@ -5,8 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity(name = "customers")
+@Entity
+@Table(name = "customers")
 public class Customer 
 {
 	@Id
