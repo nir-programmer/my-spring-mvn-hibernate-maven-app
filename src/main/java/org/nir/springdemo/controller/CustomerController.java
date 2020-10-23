c50fb26a2059e777ed5ac07c9c0a43a17a6dd4a0
nir <niritzhak10@gmail.com>
2020-10-23 14:51:25 +0300

    UPDATE THE CustomerCOntroller

--- src/main/java/org/nir/springdemo/controller/CustomerController.java
+++ src/main/java/org/nir/springdemo/controller/CustomerController.java
@@ -1,5 +1,10 @@
 package org.nir.springdemo.controller;
 
+import java.util.List;
+
+import org.nir.springdemo.dao.CustomerDAO;
+import org.nir.springdemo.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +13,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customers")
 public class CustomerController 
 {
+	//need to inject the customer dao -spring will do this 
+	@Autowired
+	private CustomerDAO customerDAO ;
+	
+	
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel)
 	{
+		//get customers from  dao
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		//add the customers to the spring mvc model
+		theModel.addAttribute("customers" , theCustomers);
+		
 		return "list-customers";
 	}
 
--- src/main/webapp/WEB-INF/view/list-customers.jsp
+++ src/main/webapp/WEB-INF/view/list-customers.jsp
@@ -8,5 +8,7 @@
 </head>
 <body>
 	<h2>Hello Nir !!!!Spring MVC</h2>
+	
+	
 </body>
 </html>
