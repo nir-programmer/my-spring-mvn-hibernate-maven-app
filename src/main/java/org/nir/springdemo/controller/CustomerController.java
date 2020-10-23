990432249d058d25eb72ad3eea896b92423f2d59
nir <niritzhak10@gmail.com>
2020-10-23 12:06:19 +0300

    update request mapping to customers

--- src/main/java/org/nir/springdemo/controller/CustomerController.java
+++ src/main/java/org/nir/springdemo/controller/CustomerController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/customer")
+@RequestMapping("/customers")
 public class CustomerController 
 {
 	@RequestMapping("/list")
